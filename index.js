ed1efe4e18bebb6a4a19f4333730e25fc0b5c663
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-07-21 21:56:29 -0300

    Data structure task

--- index.js
+++ index.js
@@ -29,4 +29,40 @@ function palindrome(string) {
   return palindromeWords;
 }
 
-console.log(palindrome("That red civic is a racecar"));
+// console.log(palindrome("That red civic is a racecar"));
+
+// Data structure task
+class Stack {
+  constructor(...number) {
+    this.items = [];
+    this.items.push(...number);
+  }
+
+  push(item) {
+    if (item) {
+      this.items.push(item);
+      return this.items;
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return "The stack is alredy empty";
+    }
+    this.items.pop();
+    return this.items;
+  }
+
+  isEmpty() {
+    if (this.items.length === 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
+// const myStack = new Stack(1, 2, 3);
+// console.log(myStack);
+// console.log(myStack.push(4));
+// console.log(myStack.pop());
+// console.log(myStack.isEmpty());
