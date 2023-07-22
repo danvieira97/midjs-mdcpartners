e7b6a14eaaba83c7340d3d61d0c50efbae7813d4
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-07-21 21:33:27 -0300

    Factorial task

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world!");
+// Factorial task
+function factorial(number) {
+  const numberArray = Array.from({ length: number }, (_, index) => index + 1);
+
+  const factorialNumber = numberArray.reduce((total, num) => total * num);
+
+  return factorialNumber;
+}
+
+console.log(factorial(5));
