23db6ee6a98e9c55b776e78b90695d1c387b6d41
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-07-21 21:41:00 -0300

    Palindrome task

--- index.js
+++ index.js
@@ -7,4 +7,26 @@ function factorial(number) {
   return factorialNumber;
 }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+// Palindrome task
+function palindrome(string) {
+  const stringArray = string.split(" ");
+
+  const palindromeWords = stringArray.filter((word) => {
+    const isPalindrome = word
+      .split("")
+      .reverse()
+      .toString()
+      .replaceAll(",", "")
+      .toLowerCase();
+
+    if (isPalindrome === word) {
+      return isPalindrome;
+    }
+  });
+
+  return palindromeWords;
+}
+
+console.log(palindrome("That red civic is a racecar"));
